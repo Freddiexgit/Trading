b4fdbeb23764cc9ca30060c3f14640d18bade6cd
freddie <freddie.xie@gmail.com>
2025-09-04 22:05:00 +1200

    commit

--- dashboard_convergence_divergence_us.py
+++ dashboard_convergence_divergence_us.py
@@ -13,10 +13,9 @@ s_str = datetime.now().strftime("%Y-%m-%d")
 if not os.path.exists(f"resource/{s_str}/us"):
     # Create the directory
     os.makedirs(f"resource/{s_str}/us")
-ec.call('nyse_and_nasdaq_top_500.csv', f'resource/{s_str}/us/ema_cv_dv_{s_str}.csv')
-ema.run(f'resource/{s_str}/us/ema_cv_dv_{s_str}.csv', f'resource/{s_str}/us/us_stock_5days_above_20days_{s_str}.csv')
-obe.order_by_ema(f'resource/{s_str}/us/us_stock_5days_above_20days_{s_str}.csv', f'resource/{s_str}/us/order_by_ema_{s_str}.csv', 20)
+# ec.call('nyse_and_nasdaq_top_500.csv', f'resource/{s_str}/us/ema_cv_dv_{s_str}.csv')
+obe.order_by_ema(f'resource/{s_str}/us/ema_cv_dv_{s_str}.csv', f'resource/{s_str}/us/order_by_ema_{s_str}.csv', 20)
 
 df_tickers = pd.read_csv(f"resource/{s_str}/us/order_by_ema_{s_str}.csv")
-# df_tickers = df_tickers[0:50]
+df_tickers = df_tickers[0:50]
 di.generate_pdf(df_tickers,f"resource/{s_str}/us/us_stock_cv_dv_indicators_{datetime.now().strftime('%Y-%m-%d-%H-%M')}.pdf","Yes","us")
--- ema_convergence_divergence.py
+++ ema_convergence_divergence.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import pandas as pd
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
-def detect_ema_convergence_divergence(ticker, period="1mo", converge_thresh=0.5, diverge_thresh=2):
+def detect_ema_convergence_divergence(ticker, period="1mo", converge_thresh=0.005, diverge_thresh=0.03):
     # Download historical data
     df = yf.download(ticker, period=period, interval="1d")
     df = df.droplevel(1, axis=1) if isinstance(df.columns, pd.MultiIndex) else df
@@ -15,15 +15,23 @@ def detect_ema_convergence_divergence(ticker, period="1mo", converge_thresh=0.5,
 
     # Distance between EMAs
     df["EMA_range"] = df[["EMA5", "EMA10", "EMA20"]].max(axis=1) - df[["EMA5", "EMA10", "EMA20"]].min(axis=1)
-
+    df.drop(index=df.index[0], inplace=True) ## remove first row as ema have same values
     # Define conditions
-    df["Converged"] = df["EMA_range"] < converge_thresh
-    df["Diverged"] = df["EMA_range"] > diverge_thresh
+    df["Converged"] = df["EMA_range"] < df["EMA10"] * converge_thresh
+    df["Diverged"] = df["EMA_range"] > df["EMA10"] * diverge_thresh
 
-    # Signal: converged yesterday, diverged today
-    df["Converge_Diverge_Signal"] = df["Converged"].shift(1) & df["Diverged"]
-    # print(df)
-    return df[df["Converge_Diverge_Signal"]]
+    if len(df[df['Converged']])> 0 and len(df[df['Diverged']])>0:
+        last_converge = df[df['Converged']].index[-1]
+        first_diverge = df[df['Diverged']].index[0]
+        latest_ema5 = df.iloc[-1]['EMA5']
+        latest_ema20 = df.iloc[-1]['EMA20']
+        if last_converge < first_diverge and latest_ema5 > latest_ema20:
+             # print(df)
+            return df
+
+    return pd.DataFrame()
+
+
 
 # Example usage
 def call(input_file, output_file):
@@ -39,3 +47,6 @@ def call(input_file, output_file):
     df2 = pd.DataFrame(result, columns=['symbol']).drop_duplicates()
     df2.to_csv(f'{output_file}', index=False)
 
+if __name__ == "__main__":
+    df = detect_ema_convergence_divergence("PHL.NZ")
+    print(df)
