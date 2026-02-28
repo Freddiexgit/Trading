001f628855b1e6667838d3804b303621dc9a101b
freddie <freddie.xie@gmail.com>
2026-03-01 10:07:04 +1300

    accumulate and structure

---
+++ resource/Utils.py
@@ -0,0 +1,12 @@
+def rsi(series, period=14):
+    delta = series.diff()
+
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
+
+    avg_gain = gain.ewm(alpha=1/period, adjust=False).mean()
+    avg_loss = loss.ewm(alpha=1/period, adjust=False).mean()
+
+    rs = avg_gain / avg_loss
+    return 100 - (100 / (1 + rs))
+
